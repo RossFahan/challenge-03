612e607c04d4612aaf47c70ece1d7ad2391f6861
Ross F <rfahan@gmail.com>
2023-06-30 20:11:49 -0600

    added confirm for lowercase and numbers

--- script.js
+++ script.js
@@ -1,31 +1,28 @@
 // Assignment Code
 var numbers = ["1", "2", "3"];
-var numbers = ["1", "2", "3"];
-var numbers = ["1", "2", "3"];
-var numbers = ["1", "2", "3"];
 
 //creating a reference to a generate button on html with the variable name generation
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-//function is writePassword
 function writePassword() {
-  //creates variable "password" and sets it equal to the return function called return "generatePassword"
   var password = generatePassword();
-  // create variable "password text" with function grabbing "id=password" in html
   var passwordText = document.querySelector("#password");
-// setting q value to password 
   passwordText.value = password;
 
 }
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 //Phase 1
 //Create a variable and get the info we need
 //confirm user for upper/lower case (CONFIRM yes? no?)
-//confirm if user would like to user numbers
+var includeLowercase = confirm("Include lowercase characters?");
+//confirm if user would like to use numbers
+var includeNumbers = confirm("Include numeric characters?");
+
+
+//confirm if user would like to use numbers
 //if yes to numbers: add the numbers to availbleChars
 //Create a pool of available characters(lowercase/uppercase/numbers/ special chars 
 // all are yes/no expect for length 
